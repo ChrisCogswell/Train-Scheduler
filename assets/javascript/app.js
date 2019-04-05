2b3bc4367b1e84edb720a2d0aabbe2812ace87e5
ChrisCogs <ccogs@yahoo.com>
2019-04-05 18:22:12 -0400

    Firebase working and showing most recent info. Need to figue out how to store multiple trains. Added background.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ var config = {
 
   var database = firebase.database();
 
-  console.log(database);
+  
 
   var trainName = "";
   var destination = "";
@@ -20,3 +20,35 @@ var config = {
   var nextArrival = "";
   var minsAway = "";
 
+$("#add-train").on("click", function(event){
+    event.preventDefault();
+
+    trainName = $("#name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    nextArrival = $("#time-input").val().trim();
+
+    database.ref().set({
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency,
+        nextArrival: nextArrival
+    });
+});
+
+database.ref().on("value", function(snapshot){
+
+    console.log(snapshot.val());
+    console.log(snapshot.val().trainName);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().frequency);
+    console.log(snapshot.val().nextArrival);
+
+    $("#name-display").text(snapshot.val().trainName);
+    $("#destination-display").text(snapshot.val().destination);
+    $("#frequency-display").text(snapshot.val().frequency);
+    $("#arrival-display").text(snapshot.val().nextArrival);
+
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
